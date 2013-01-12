fd7198b4fce89f5203c135108ec6f697578134ee
Ben Bray <archon68@gmail.com>
2013-01-12 13:50:42 -0500

    Added ThirdClass with completely serious code.

---
+++ src/adambots/git/tests/ThirdClass.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package adambots.git.tests;
+
+/**
+ *
+ * @author Ben
+ */
+public class ThirdClass {
+    
+    public static final String SERIOUS_STRING = "TROLOLOLO";
+    
+    public ThirdClass(){
+	if(true == false){
+	    System.out.println(SERIOUS_STRING);
+	}	
+    }
+}
