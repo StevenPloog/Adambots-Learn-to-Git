b1d801e9c4197ad78228f80cb1b9940b9ca438fc
Ben Bray <archon68@gmail.com>
2013-01-12 14:21:51 -0500

    Added Elseif

--- src/adambots/git/tests/ThirdClass.java
+++ src/adambots/git/tests/ThirdClass.java
@@ -13,8 +13,11 @@ public class ThirdClass {
     public static final String SERIOUS_STRING = "TROLOLOLO";
     
     public ThirdClass(){
+	
 	if(true == false){
 	    System.out.println(SERIOUS_STRING);
+	} else if(!true || false){
+	    System.err.println("wat");
 	}	
     }
 }
