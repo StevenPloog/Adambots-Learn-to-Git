277c6864362d07df5bf0ded3cc0bd3738af8cdbf
Steven <Steven@10.0.1.7>
2013-01-12 13:50:04 -0500

    Added the body and constructor of the class.

---
+++ src/adambots/git/tests/SecondClass.java
@@ -0,0 +1,14 @@
+package adambots.git.tests;
+
+/**
+ * @author Steven
+ */
+
+public class SecondClass {
+    
+    private String _message;
+    
+    public SecondClass(String message) {
+	_message = message;
+    }
+}
