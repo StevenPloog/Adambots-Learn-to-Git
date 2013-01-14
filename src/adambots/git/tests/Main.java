a211e64a5df78e033c25c2e4d16916d77ddc8117
Steven <Steven@10.0.1.7>
2013-01-14 16:44:26 -0500

    Deleted everything. Lets hope we can revert

--- src/adambots/git/tests/Main.java
+++ src/adambots/git/tests/Main.java
@@ -1,19 +1,4 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-package adambots.git.tests;
-
+//LOL I DELETED EVERYTHING LOLOLOLOOLOLOLOL
 /**
- *
- * @author Steven
+ * @STEVEN
  */
-public class Main {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
-}
