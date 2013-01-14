f047d078651151d5fddf7e8d353bc3c18cebcc0b
Steven <Steven@Steven-PC>
2013-01-14 16:43:30 -0500

    Revert "Added print line for hello world."
    This reverts commit 5e79f697ac3f76adae24ecb7e56545c1623195b6.

--- src/adambots/git/tests/Main.java
+++ src/adambots/git/tests/Main.java
@@ -1,6 +1,11 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package adambots.git.tests;
 
 /**
+ *
  * @author Steven
  */
 public class Main {
@@ -9,6 +14,6 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        // TODO code application logic here
     }
 }
